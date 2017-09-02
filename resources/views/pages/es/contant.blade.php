c46682a22cc9b15b48775f48edb4094d19d66e0f
Gerardo Hernández <edgewl2@gmail.com>
2017-09-02 16:02:01 -0600

    Add contant page

---
+++ resources/views/pages/es/contant.blade.php
@@ -0,0 +1,95 @@
+<div class="container">
+
+  <!-- Section: Contact v.2 -->
+  <section id="contact" class="section mb-5">
+
+    <!-- Section heading -->
+    <h3 class="text-center mb-5 mt-5 pt-5 font-up font-bold grey-text-2 wow fadeIn" data-wow-delay="0.2s">Contáctenos</h3>
+
+    <!-- Section sescription -->
+    <p class="section-description mb-5 wow fadeIn" data-wow-delay="0.2s">Puede contactarnos o enviar un mensaje. Con gusto responderemos.</p>
+
+    <div class="row wow fadeIn" data-wow-delay="0.4s">
+
+      <!-- Grid column -->
+      <div class="col-md-8 col-lg-9">
+        <form>
+          <!-- Grid row -->
+          <div class="row">
+            <!-- Grid column -->
+            <div class="col-md-6">
+              <div class="md-form">
+                <div class="md-form">
+                  <input type="text" id="form41" class="form-control">
+                  <label for="form41" class="">Nombre:</label>
+                </div>
+              </div>
+            </div>
+
+            <!-- Grid column -->
+            <div class="col-md-6">
+              <div class="md-form">
+                <div class="md-form">
+                  <input type="text" id="form52" class="form-control">
+                  <label for="form52" class="">Email:</label>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- Grid row -->
+
+          <!-- Grid row -->
+          <div class="row">
+            <div class="col-md-12">
+              <div class="md-form">
+                <input type="text" id="form51" class="form-control">
+                <label for="form51" class="">Asunto:</label>
+              </div>
+            </div>
+          </div>
+          <!-- Grid row -->
+
+          <!-- Grid row -->
+          <div class="row">
+            <!-- Grid column -->
+            <div class="col-md-12">
+
+              <div class="md-form">
+                <textarea type="text" id="form76" class="md-textarea"></textarea>
+                <label for="form76">Mensaje:</label>
+              </div>
+
+            </div>
+          </div>
+          <!-- Grid row -->
+        </form>
+
+        <div class="center-on-small-only mb-5">
+          <a class="btn btn-rounded btn-light-green-2">Enviar</a>
+        </div>
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-4 col-lg-3">
+        <ul class="contact-icons float-md-right">
+          <li><i class="fa fa-map-marker fa-2x"></i>
+            <p>Colonia Universidad, de la rotonda 6c. al Este, 20 vr. al Sur. León</p>
+          </li>
+
+          <li><i class="fa fa-phone fa-2x"></i>
+            <p>(505) 8885-8659</p>
+          </li>
+
+          <li><i class="fa fa-envelope fa-2x"></i>
+            <p>consultoreshbs@gmail.com</p>
+          </li>
+        </ul>
+      </div>
+      <!-- Grid column -->
+
+    </div>
+  </section>
+  <!-- Section: Contact v.2 -->
+
+</div>
