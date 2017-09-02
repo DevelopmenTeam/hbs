6dd9ce4fea8cc39f341f4534c1ccae19edf7cf55
Luis Solorzano <luisnic20@gmail.com>
2017-09-02 16:08:41 -0600

    add banner image

--- resources/views/globals/header.blade.php
+++ resources/views/globals/header.blade.php
@@ -14,11 +14,16 @@
           <div class="row pt-5 mt-3">
             <div class="col-md-12 white-text text-center smooth-scroll">
               <div class="wow fadeInDown" data-wow-delay="0.2s">
-                <h3 class="display-3 font-bold white-text mb-2">HBS Consultores Turisticos</h3>
+
+
+                <img src="{{ asset('assets/media/logo/header_hbs.png') }}" style="width: 100%;"/>
+
+
+                {{--<h3 class="display-3 font-bold white-text mb-2">HBS Consultores Turisticos</h3>
                 <hr class="hr-light my-4">
                 <h4 class="subtext-header mt-2 mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti consequuntur, nihil voluptatem.</h4>
               </div>
-              <a href="#about" data-offset="90" class="btn btn-rounded btn-light-green-2 wow fadeInUp" data-wow-delay="0.2s"><i class="fa fa-home"></i> Visit us</a>
+              <a href="#about" data-offset="90" class="btn btn-rounded btn-light-green-2 wow fadeInUp" data-wow-delay="0.2s"><i class="fa fa-home"></i> Visit us</a>--}}
             </div>
           </div>
         </div>
