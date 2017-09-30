0c72d8bcd738b7c99e8bca4bf50a525511adcc3a
Luis Solorzano <luisnic20@gmail.com>
2017-09-30 11:38:11 -0600

    kernel

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Route::get('/mailable', function () {
 //Auth::routes();
 
 //Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
